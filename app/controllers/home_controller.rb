6eb561bf337684888a1c7139fe0a99c3e6c57a3d
ralph <raissorad@gmail.com>
2014-11-13 14:01:06 +0800

    init

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,13 @@
+class HomeController < ApplicationController
+
+	def index
+
+	api_key = 'Q7FGWJCFD9UYURUXR'
+	url = 'http://41cc9a70.ngrok.com/guessthissong.mp3'
+	req = HTTParty.post('http://developer.echonest.com/api/v4/track/upload',
+		:body => {"url" => url,
+				 "api_key" => api_key} )
+	render json: req.body
+	end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Echoprint::Application.config.secret_token = 'aebcf6fa5297f7396c63c6df7a48ae04873a4bc7ff4211a9f85730bc9a6c4650bf939041dfa2f0938791005e1cc01de7a91febe0e36fd2704f331fb4efbf640a'
