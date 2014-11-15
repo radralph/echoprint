c21db0ce318c230883ffc25d438a89af0903d520
ralph <raissorad@gmail.com>
2014-11-15 18:29:17 +0800

    list TODO

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ApplicationController
 
 	def index
 
+	file = params[:file]
 	api_key = 'Q7FGWJCFD9UYURUXR'
 	url = 'http://41cc9a70.ngrok.com/guessthissong.mp3'
 	req = HTTParty.post('http://developer.echonest.com/api/v4/track/upload',
@@ -9,6 +10,10 @@ class HomeController < ApplicationController
 				 "api_key" => api_key} )
 	#render json: req.body
 	#render json: {'success' => true}
+
+	##TODO:
+	##file upload
+	##after analyzing, delete file in directory
 
 	end
 
