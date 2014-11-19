b63707daf5777e30f6e1423a143951da2bf39a5a
ralph <raissorad@gmail.com>
2014-11-19 15:11:39 +0800

    list artist genre

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -13,22 +13,29 @@ class HomeController < ApplicationController
 	directory = "public/" ; path = File.join(directory, name)
 	File.open(path,"wb") { |f| f.write(params[:file].read) } 
 
+	logger.info url
+		req = HTTParty.post('http://developer.echonest.com/api/v4/track/upload',
+		:body => {"url" => url,
+				 "api_key" => api_key} )
+
+
+
 	##Set Echoprint
 	site = 'http://2789443a.ngrok.com/'
 	api_key = 'PSDYDEXB3L5COAXNK'
 	url = site+name
-	req = HTTParty.post('http://developer.echonest.com/api/v4/track/upload',
-		:body => {"url" => url,
-				 "api_key" => api_key} )
-	render json: req.body
-	#render json: {'success' => true}
+
+	#render json: req.body
+	render json: {'success' => true}
 	#File.delete(Rails.root + 'public/'+name)
-	logger.info url
+	
 	#search similar by genre:
 	##http://developer.echonest.com/api/v4/genre/similar?api_key=Q7FGWJCFD9UYURUXR&name=acid+jazz&bucket=description
 	#suggest music
 	#http://developer.echonest.com/api/v4/artist/suggest?api_key=PSDYDEXB3L5COAXNK&name=rad&results=5
-
+	#http://developer.echonest.com/api/v4/artist/search?api_key=PSDYDEXB3L5COAXNK&format=json&name=radiohead&results=1
+	#list artists' genre
+	#http://developer.echonest.com/api/v4/artist/terms?api_key=PSDYDEXB3L5COAXNK&name=radiohead&format=json
 
 	end
 
